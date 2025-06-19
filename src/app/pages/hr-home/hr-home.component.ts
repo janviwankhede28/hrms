210011e613e71dfe9f27a06e1dff14da3b2ebda6
Arjun Rajput <arjunrajput7531@gmail.com>
2025-06-19 18:02:34 +0530

    changes by ak47

--- src/app/pages/hr-home/hr-home.component.ts
+++ src/app/pages/hr-home/hr-home.component.ts
@@ -1,38 +1,4 @@
 import { HolidayService } from './../../../services/holidays.service';
-// import { Component, OnInit, OnDestroy } from '@angular/core';
-
-// @Component({
-//   selector: 'app-hr-home',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './hr-home.component.html',
-//   styleUrl: './hr-home.component.css'
-// })
-// export class HrHomeComponent implements OnInit, OnDestroy {
-
-//   timeString: string = '';
-//   private intervalId: any;
-
-//   ngOnInit(): void {
-//     this.updateTime();
-//     this.intervalId = setInterval(() => this.updateTime(), 1000);
-//   }
-
-//   ngOnDestroy(): void {
-//     clearInterval(this.intervalId);
-//   }
-
-//   updateTime(): void {
-//     const now = new Date();
-//     const hours = String(now.getHours()).padStart(2, '0');
-//     const minutes = String(now.getMinutes()).padStart(2, '0');
-//     const seconds = String(now.getSeconds()).padStart(2, '0');
-//     this.timeString = `${hours} : ${minutes} : ${seconds}`;
-//   }
-// }
-
-
-
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
